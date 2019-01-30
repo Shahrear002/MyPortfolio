346f1a2262903d0047016bbe12fc87a33b8435e2
Shahrear002 <shabuz2900@gmail.com>
2019-01-30 21:19:24 +0600

    header scrolling

--- javascript/main.js
+++ javascript/main.js
@@ -24,4 +24,14 @@ jQuery(document).ready(function(){
             pageAnchorBuilder: false
         }
     });
+    $(window).scroll(function(){
+        var top = $(window).scrollTop();
+        if(top >= 60){
+            $("header").addClass('secondary');
+        }
+        else
+            if($("header").hasClass('secondary')){
+                $("header").removeClass('secondary');
+            }
+    });
 });
